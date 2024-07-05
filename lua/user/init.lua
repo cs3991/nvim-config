4e5c418e1516dc885c52c86e5241fea05356a5c3
Cédric <simardced31@gmail.com>
2024-07-05 23:54:54 +0200

    update user/init.lua and add dart lsp

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,2 +1,10 @@
 require("user.remap")
 
+vim.opt.scrolloff = 8
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.smartindent = true
+
